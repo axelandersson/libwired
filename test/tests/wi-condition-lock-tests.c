7b426a91c3920d6d74de0bc9a66c5bc66e4dcffe
Axel Andersson <axanders@microsoft.com>
2015-01-30 14:11:56 +0100

    Add wi-condition-lock-tests, wi-lock-tests, wi-readwrite-lock-tests, wi-recursive-lock-tests

---
+++ test/tests/wi-condition-lock-tests.c
@@ -0,0 +1,91 @@
+/*
+ *  Copyright (c) 2015 Axel Andersson
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <wired/wired.h>
+
+WI_TEST_EXPORT void                     wi_test_condition_lock_creation(void);
+WI_TEST_EXPORT void                     wi_test_condition_lock_runtime_functions(void);
+WI_TEST_EXPORT void                     wi_test_condition_lock_locking(void);
+
+
+void wi_test_condition_lock_creation(void) {
+#ifdef WI_PTHREADS
+    wi_condition_lock_t     *lock;
+    
+    lock = wi_autorelease(wi_condition_lock_init(wi_condition_lock_alloc()));
+    
+    WI_TEST_ASSERT_NOT_NULL(lock, "");
+    WI_TEST_ASSERT_EQUALS(wi_condition_lock_condition(lock), 0, "");
+
+    lock = wi_autorelease(wi_condition_lock_init_with_condition(wi_condition_lock_alloc(), 1));
+    
+    WI_TEST_ASSERT_NOT_NULL(lock, "");
+    WI_TEST_ASSERT_EQUALS(wi_condition_lock_condition(lock), 1, "");
+#endif
+}
+
+
+
+void wi_test_condition_lock_runtime_functions(void) {
+#ifdef WI_PTHREADS
+    wi_condition_lock_t     *lock;
+    
+    lock = wi_autorelease(wi_condition_lock_init(wi_condition_lock_alloc()));
+    
+    WI_TEST_ASSERT_EQUALS(wi_runtime_id(lock), wi_condition_lock_runtime_id(), "");
+    
+    WI_TEST_ASSERT_NOT_EQUALS(wi_string_index_of_string(wi_description(lock), WI_STR("wi_condition_lock_t"), 0), WI_NOT_FOUND, "");
+#endif
+}
+
+
+
+void wi_test_condition_lock_locking(void) {
+#ifdef WI_PTHREADS
+    wi_condition_lock_t     *lock;
+    wi_boolean_t            result;
+    
+    lock = wi_autorelease(wi_condition_lock_init_with_condition(wi_condition_lock_alloc(), 1));
+    
+    wi_condition_lock_lock(lock);
+    wi_condition_lock_unlock(lock);
+
+    wi_condition_lock_lock_when_condition(lock, 1, 1.0);
+    wi_condition_lock_unlock(lock);
+
+    result = wi_condition_lock_try_lock(lock);
+    
+    WI_TEST_ASSERT_TRUE(result, "");
+    
+    wi_condition_lock_unlock(lock);
+    
+    result = wi_condition_lock_try_lock_when_condition(lock, 1);
+    
+    WI_TEST_ASSERT_TRUE(result, "");
+    
+    wi_condition_lock_unlock(lock);
+#endif
+}
---
+++ test/tests/wi-readwrite-lock-tests.c
@@ -0,0 +1,85 @@
+/*
+ *  Copyright (c) 2015 Axel Andersson
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <wired/wired.h>
+
+WI_TEST_EXPORT void                     wi_test_readwrite_lock_creation(void);
+WI_TEST_EXPORT void                     wi_test_readwrite_lock_runtime_functions(void);
+WI_TEST_EXPORT void                     wi_test_readwrite_lock_locking(void);
+
+
+void wi_test_readwrite_lock_creation(void) {
+#ifdef WI_PTHREADS
+    wi_readwrite_lock_t     *lock;
+    
+    lock = wi_autorelease(wi_readwrite_lock_init(wi_readwrite_lock_alloc()));
+    
+    WI_TEST_ASSERT_NOT_NULL(lock, "");
+#endif
+}
+
+
+
+void wi_test_readwrite_lock_runtime_functions(void) {
+#ifdef WI_PTHREADS
+    wi_readwrite_lock_t     *lock;
+    
+    lock = wi_autorelease(wi_readwrite_lock_init(wi_readwrite_lock_alloc()));
+    
+    WI_TEST_ASSERT_EQUALS(wi_runtime_id(lock), wi_readwrite_lock_runtime_id(), "");
+    
+    WI_TEST_ASSERT_NOT_EQUALS(wi_string_index_of_string(wi_description(lock), WI_STR("wi_readwrite_lock_t"), 0), WI_NOT_FOUND, "");
+#endif
+}
+
+
+
+void wi_test_readwrite_lock_locking(void) {
+#ifdef WI_PTHREADS
+    wi_readwrite_lock_t     *lock;
+    wi_boolean_t            result;
+    
+    lock = wi_autorelease(wi_readwrite_lock_init(wi_readwrite_lock_alloc()));
+    
+    wi_readwrite_lock_read_lock(lock);
+    wi_readwrite_lock_unlock(lock);
+    
+    wi_readwrite_lock_write_lock(lock);
+    wi_readwrite_lock_unlock(lock);
+    
+    result = wi_readwrite_lock_try_read_lock(lock);
+    
+    WI_TEST_ASSERT_TRUE(result, "");
+    
+    wi_readwrite_lock_unlock(lock);
+    
+    result = wi_readwrite_lock_try_write_lock(lock);
+    
+    WI_TEST_ASSERT_TRUE(result, "");
+    
+    wi_readwrite_lock_unlock(lock);
+#endif
+}
